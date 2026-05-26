69c0da7b0ea0ece7944e3b29eaf5c18386d677a0
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-05-26 15:34:11 -0600

    update routing to avoid adblocker errors

--- server/server.go
+++ server/server.go
@@ -29,6 +29,7 @@ func New(addr string, allowedOrigins []string, application *app.Application, aut
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger(slog.Default()))
 	r.Use(chimiddleware.Recoverer)
+	r.Use(middleware.StripAPIPrefix("/api/job-search"))
 	r.Group(func(r chi.Router) {
 		r.Use(middleware.Tracing(application.Tracer))
 		registerRoutes(r, application, authMiddleware)
